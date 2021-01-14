1abee400ac652dbe0e35d005af957fc6ce1d88de
KELECHIDAVIS <75586620+KELECHIDAVIS@users.noreply.github.com>
2021-01-14 11:26:01 -0800

    Add files via upload

---
+++ SnakeMain.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*; 
+import java.util.Random;
+public class SnakeMain {
+
+	public static void main(String[] args) {
+		
+		new GameFrame(); 
+		
+
+	}
+
+}
